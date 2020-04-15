1722a967c8c2e7d4d21cf776846dcfa8f8c10197
Kari Merikanto <karimerikanto@localhost.localdomain>
2020-04-15 11:11:22 +0300

    Add an error

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -85,6 +85,7 @@ export default function Home() {
   return (
     <div className="Home">
       {isAuthenticated ? renderNotes() : renderLander()}
+      {isAuthenticated.none.no}
     </div>
   );
 }
