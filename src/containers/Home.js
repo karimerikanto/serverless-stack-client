2fcfb56a456232a137f75f105af0b376f1661a8f
Kari Merikanto <karimerikanto@localhost.localdomain>
2020-04-15 11:14:16 +0300

    Fix created error

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -85,7 +85,6 @@ export default function Home() {
   return (
     <div className="Home">
       {isAuthenticated ? renderNotes() : renderLander()}
-      {isAuthenticated.none.no}
     </div>
   );
 }
